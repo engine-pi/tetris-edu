9be304b69265ca91f79aff4fa4cbdd26317bc300
Josef Friedrich <josef@friedrich.rocks>
2024-06-07 09:23:54 +0200

    InGameScene

--- src/main/java/de/pirckheimer_gymnasium/tetris/scenes/IngameScene.java
+++ src/main/java/de/pirckheimer_gymnasium/tetris/scenes/IngameScene.java
@@ -1,12 +1,34 @@
 package de.pirckheimer_gymnasium.tetris.scenes;
 
 import de.pirckheimer_gymnasium.tetris.Tetris;
+import de.pirckheimer_gymnasium.tetris.tetrominos.Grid;
+import de.pirckheimer_gymnasium.tetris.tetrominos.Tetromino;
+
+import java.util.Random;
 
 public class IngameScene extends BaseScene
 {
+    private Grid grid;
+    private static Random random = new Random();
+
+    private int nextTetromino;
+
+    private Tetromino tetromino;
+
+    private Tetromino previewTetromino;
+
     public IngameScene()
     {
         super("ingame");
+    }
+
+    private void createNextTetromino() {
+        if (previewTetromino == null) {
+            nextTetromino = random.nextInt(7);
+        }
+
+        tetromino = Tetromino.create(this, grid, "L", 4, 16);
+        nextTetromino = random.nextInt(7);
     }
 
     public static void main(String[] args)
